95592884d9fa8cd6b94e48f528cf0bc9dff4f73f
Wahab Shah <shahw@sheridancollege.ca>
2021-03-25 20:00:06 -0400

    Tutorials Page UI Completed

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import Register from './src/Register'
 import HomePage from './src/HomePage'
+import TutorialsPage from "./src/TutorialsPage";
 
 
 const Stack = createStackNavigator();
@@ -14,8 +15,9 @@ function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Register" component={Register} />
-        <Stack.Screen name="HomePage" component={HomePage} />
+          <Stack.Screen name="TutorialsPage" component={TutorialsPage} />
+          <Stack.Screen name="Register" component={Register} />
+          <Stack.Screen name="HomePage" component={HomePage} />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- src/TutorialsPage.js
+++ src/TutorialsPage.js
@@ -1,23 +1,139 @@
 import * as React from 'react';
-import {Button, ThemeProvider, Input, Text} from 'react-native-elements'
-import {
-    StyleSheet,
-    View,
-} from 'react-native';
+import DropDownPicker from 'react-native-dropdown-picker';
+import {Button, ThemeProvider, Input, Text, Avatar} from 'react-native-elements';
+
+import { FlatList, StyleSheet, View, TouchableHighlight, ScrollView } from 'react-native';
+
+
 
 function TutorialsPage() {
     return (
-        <View style={styles.body}>
-            <Text h1>VALORANT UNIVERSITY</Text>
-        </View>
+
+
+        <ScrollView style={styles.container}>
+
+            <View style={styles.body}>
+
+                <Text h3>   TUTORIALS</Text>
+
+            </View>
+
+            <View style={styles.header}>
+                <DropDownPicker
+                    items={[
+                        {label: 'Haven', value: 'Haven'},
+                        {label: 'Bind', value: 'Bind'},
+                    ]}
+                    defaultIndex={0}
+                    placeholder="Map"
+                    containerStyle={{width: '30%', height: 40}}
+                    onChangeItem={item => console.log(item.label, item.value)}
+                />
+                <DropDownPicker
+                    items={[
+                        {label: 'Sova', value: 'Sova'},
+                        {label: 'Viper', value: 'Viper'},
+                    ]}
+                    defaultIndex={0}
+                    placeholder="Agent"
+                    containerStyle={{width: '30%', height: 40}}
+                    onChangeItem={item => console.log(item.label, item.value)}
+                />
+                <DropDownPicker
+                    items={[
+                        {label: 'Attack', value: 'Attack'},
+                        {label: 'Defense', value: 'Defense'},
+                    ]}
+                    defaultIndex={0}
+                    placeholder="Phase"
+                    containerStyle={{width: '30%', height: 40}}
+                    onChangeItem={item => console.log(item.label, item.value)}
+                />
+            </View>
+
+            <View style={styles.body}>
+
+                <FlatList
+                    ItemSeparatorComponent={
+                        Platform.OS !== 'android' &&
+                        (({ highlighted }) => (
+                            <View
+                                style={[
+                                    style.separator,
+                                    highlighted && { marginLeft: 0 }
+                                ]}
+                            />
+                        ))
+                    }
+                    data={[{ map: 'Bind', agent: 'Sova', phase: 'Attack', author: 'Wahby', key: 'Bind' },
+                        { map: 'Ascent', agent: 'Sova', phase: 'Attack',  author: 'Wahby', key: 'Ascent' },
+                        { map: 'Haven', agent: 'Sova', phase: 'Defense',  author: 'Wahby', key: 'Haven' },
+                        { map: 'Icebox', agent: 'Sova', phase: 'Defense',  author: 'Wahby', key: 'Icebox' },
+                        { map: 'Split', agent: 'Sova', phase: 'Attack',  author: 'Wahby', key: 'Split' }]}
+
+                    renderItem={({ item, index, separators }) => (
+                        <TouchableHighlight
+                            key={item.key}
+                            onPress={() => (console.log(item.key))}
+                            onShowUnderlay={separators.highlight}
+                            onHideUnderlay={separators.unhighlight}>
+                            <View style={styles.item}>
+                                <Avatar
+                                    size="medium"
+                                    title="MAP"
+                                    rounded
+                                    activeOpacity={1}
+                                />
+                                <Avatar
+                                    size="medium"
+                                    title="Agent"
+                                    rounded
+                                    activeOpacity={1}
+                                />
+                                <Text style={styles.itemTitle}>    {item.phase}</Text>
+                                <Text style={styles.itemSubtitle}>       Created By: {item.author}</Text>
+                            </View>
+                        </TouchableHighlight>
+                    )}
+                />
+
+            </View>
+
+
+
+        </ScrollView>
+
     );
 }
 const styles = StyleSheet.create({
-    body:{
-        flex: 1,
+    header:{
+        flexDirection:'row',
         backgroundColor: '#ffffff',
-        alignItems: 'center',
         justifyContent: 'center',
+        zIndex: 1,
+        marginBottom:'5%'
     },
+    container:{
+        backgroundColor: '#FFFFFF',
+
+    },
+    body:{
+        backgroundColor: '#FFFFFF',
+        marginBottom:'5%'
+    },
+    item:{
+        flexDirection:'row',
+        backgroundColor: '#F1F1F1',
+        padding: '5%',
+        margin: 5
+    },
+    itemTitle:{
+        paddingTop: '3%',
+        fontSize: 20
+    },
+    itemSubtitle:{
+        paddingTop: '4%',
+        fontSize: 15
+    }
 });
 export default TutorialsPage;
