054b01c60a438461c55095773217090a356eb7e3
Wahab Shah <shahw@sheridancollege.ca>
2021-04-15 18:47:47 -0400

    tutorials page work in progess 1

--- src/TutorialsPage.js
+++ src/TutorialsPage.js
@@ -1,107 +1,33 @@
-import * as React from 'react';
-import {Button, ThemeProvider, Input, Text, Avatar} from 'react-native-elements';
-
-import { FlatList, StyleSheet, View, TouchableHighlight, ScrollView } from 'react-native';
-
+import React, { useState, useEffect } from 'react';
+import {ActivityIndicator, FlatList, View, Text, TouchableHighlight} from 'react-native';
+import { database } from '../Setup';
 
 
 function TutorialsPage() {
+
+
+    const tutorials = database().ref('tutorials').limitToFirst(10).once('value')
+    console.log(tutorials.length)
+    // database()
+    //     .ref('/tutorials')
+    //     .once('value')
+    //     .then(snapshot => {
+    //         console.log('User data: ', snapshot.val());
+    //     });
+
     return (
-
-
-        <ScrollView style={styles.container}>
-
-            <View style={styles.body}>
-
-                <Text h3>   TUTORIALS</Text>
-
-            </View>
-
-
-
-            <View style={styles.body}>
-
-                <FlatList
-                    ItemSeparatorComponent={
-                        Platform.OS !== 'android' &&
-                        (({ highlighted }) => (
-                            <View
-                                style={[
-                                    style.separator,
-                                    highlighted && { marginLeft: 0 }
-                                ]}
-                            />
-                        ))
-                    }
-                    data={[{ map: 'Bind', agent: 'Sova', phase: 'Attack', author: 'Wahby', key: 'Bind' },
-                        { map: 'Ascent', agent: 'Sova', phase: 'Attack',  author: 'Wahby', key: 'Ascent' },
-                        { map: 'Haven', agent: 'Sova', phase: 'Defense',  author: 'Wahby', key: 'Haven' },
-                        { map: 'Icebox', agent: 'Sova', phase: 'Defense',  author: 'Wahby', key: 'Icebox' },
-                        { map: 'Split', agent: 'Sova', phase: 'Attack',  author: 'Wahby', key: 'Split' }]}
-
-                    renderItem={({ item, index, separators }) => (
-                        <TouchableHighlight
-                            key={item.key}
-                            onPress={() => (console.log(item.key))}
-                            onShowUnderlay={separators.highlight}
-                            onHideUnderlay={separators.unhighlight}>
-                            <View style={styles.item}>
-                                <Avatar
-                                    size="medium"
-                                    title="MAP"
-                                    rounded
-                                    activeOpacity={1}
-                                />
-                                <Avatar
-                                    size="medium"
-                                    title="Agent"
-                                    rounded
-                                    activeOpacity={1}
-                                />
-                                <Text style={styles.itemTitle}>    {item.phase}</Text>
-                                <Text style={styles.itemSubtitle}>       Created By: {item.author}</Text>
-                            </View>
-                        </TouchableHighlight>
-                    )}
-                />
-
-            </View>
-
-
-
-        </ScrollView>
-
+        <View>
+         <FlatList
+             data={tutorials}
+             renderItem={({ item }) => (
+                 <TouchableHighlight onPress={() => {}}>
+                     <Text>
+                         {item.Title}
+                     </Text>
+                 </TouchableHighlight>
+             )}
+         />
+        </View>
     );
 }
-const styles = StyleSheet.create({
-    header:{
-        flexDirection:'row',
-        backgroundColor: '#ffffff',
-        justifyContent: 'center',
-        zIndex: 1,
-        marginBottom:'5%'
-    },
-    container:{
-        backgroundColor: '#FFFFFF',
-
-    },
-    body:{
-        backgroundColor: '#FFFFFF',
-        marginBottom:'5%'
-    },
-    item:{
-        flexDirection:'row',
-        backgroundColor: '#F1F1F1',
-        padding: '5%',
-        margin: 5
-    },
-    itemTitle:{
-        paddingTop: '3%',
-        fontSize: 20
-    },
-    itemSubtitle:{
-        paddingTop: '4%',
-        fontSize: 15
-    }
-});
 export default TutorialsPage;
