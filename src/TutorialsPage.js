c4c80e967d4539cfbe7c271d8672bc36585e5cec
Wahab Shah <shahw@sheridancollege.ca>
2021-04-16 14:51:10 -0400

    refreshing working on tutorialspage

--- src/TutorialsPage.js
+++ src/TutorialsPage.js
@@ -1,6 +1,14 @@
 import React, {useState, useEffect, Component} from 'react';
-import {NavigationContainer} from "react-navigation";
-import {ActivityIndicator, FlatList, View, Text, TouchableHighlight, StyleSheet, RefreshControl} from 'react-native';
+import {
+    ActivityIndicator,
+    FlatList,
+    View,
+    Text,
+    TouchableHighlight,
+    StyleSheet,
+    RefreshControl,
+    ScrollView
+} from 'react-native';
 import {Header, Left, Right, Body, Title, Button, Icon} from 'native-base'
 import { database } from '../Setup';
 import YoutubePlayer from "react-native-youtube-iframe";
@@ -9,6 +17,22 @@ function TutorialsPage({navigation}){
 
     const [loading, setLoading] = useState(true); // Set loading to true on component mount
     const [tutorials, setTutorials] = useState([]); // Initial empty array of users
+    const [refreshing, setRefreshing] = useState(false);
+
+    const onRefresh = () =>{
+        setRefreshing(true)
+        database().ref('tutorials').once('value').then((snapshot) => {
+            const tutorials = [];
+
+            snapshot.forEach((child) => {
+                tutorials.push(child.val());
+            });
+
+            setTutorials(tutorials);
+            setLoading(false);
+        })
+        setRefreshing(false);
+    }
 
     useEffect(() => {
         let subscriber = database()
@@ -35,39 +59,69 @@ function TutorialsPage({navigation}){
 
     return (
         <View style={styles.container}>
-            <Header>
-                {/*<Left>*/}
+            {/*<Left>*/}
 
-                {/*    <Button transparent icon onPress={() => }>*/}
-                {/*        <Icon name="refresh"/>*/}
-                {/*    </Button>*/}
-                {/*</Left>*/}
-                <Right>
+            {/*    <Button transparent icon onPress={() => }>*/}
+            {/*        <Icon name="refresh"/>*/}
+            {/*    </Button>*/}
+            {/*</Left>*/}
 
-                    <Button transparent icon onPress={() => navigation.navigate('UploadTutorial')}>
-                        <Icon name="add"/>
-                    </Button>
-                </Right>
-            </Header>
-            <FlatList
-                data={tutorials}
-                renderItem={({item}) =>
-                    <TouchableHighlight>
-                        <View style={styles.listItem}>
-                            <View >
-                                <YoutubePlayer height={225} play={false} videoId={item.Link} />
+
+            <Button block onPress={() => navigation.navigate('mainMenu')}>
+                <Text>Create Tutorial</Text>
+            </Button>
+
+
+            {/*<FlatList*/}
+            {/*    data={tutorials}*/}
+            {/*    renderItem={({item}) =>*/}
+
+
+            <ScrollView
+                        refreshControl={
+                            <RefreshControl
+                                refreshing={refreshing}
+                                onRefresh={onRefresh}
+                            />
+                        }>
+                {
+                    tutorials.map((item) => {
+                        return(
+                            <View key={item.Id} style={styles.listItem}>
+                                <View>
+                                    <YoutubePlayer height={225} play={false} videoId={item.Link}/>
+                                </View>
+                                <View style={styles.title}>
+                                    <Text style={styles.text}>
+                                        {item.Title}
+                                    </Text>
+                                </View>
                             </View>
-                            <View style={styles.title}>
-                                <Text style={styles.text}>
-                                    {item.Title}
-                                </Text>
-                            </View>
-                        </View>
-                    </TouchableHighlight>
+                        )
+                    })
                 }
-                keyExtractor={(item, index) => index.toString()}
 
-            />
+
+                    {/*<TouchableHighlight>*/}
+                    {/*    <View style={styles.listItem}>*/}
+                    {/*        <View>*/}
+                    {/*            <YoutubePlayer height={225} play={false} videoId={item.Link}/>*/}
+                    {/*        </View>*/}
+                    {/*        <View style={styles.title}>*/}
+                    {/*            <Text style={styles.text}>*/}
+                    {/*                {item.Title}*/}
+                    {/*            </Text>*/}
+                    {/*        </View>*/}
+                    {/*    </View>*/}
+                    {/*</TouchableHighlight>*/}
+
+            </ScrollView>
+
+
+                {/*}*/}
+                {/*keyExtractor={(item, index) => index.toString()}*/}
+
+            {/*/>*/}
         </View>
     );
 }
